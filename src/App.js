e2bb246065a5e09c6734b1b9331caded7085bd1b
Владислав <vlad.maniak@gmail.com>
2020-11-29 10:45:36 +0200

    src folder with globalStyles, fixtures folder and jumbotrone folder in components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from "./components/jumbotrone";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Jumbotron.Container>
+                {jumboData.map((el) => (
+                    <Jumbotron key={el.id} direction={el.direction}>
+                        <Jumbotron.Pane>
+                            <Jumbotron.Title>
+                                {el.title}
+                            </Jumbotron.Title>
+                            <Jumbotron.SubTitle>
+                                {el.subTitle}
+                            </Jumbotron.SubTitle>
+                        </Jumbotron.Pane>
+
+                        <Jumbotron.Pane>
+                            <Jumbotron.Image src={el.image} alt={el.alt}/>
+                        </Jumbotron.Pane>
+                    </Jumbotron>
+                ))}
+            </Jumbotron.Container>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css'
+import App from './App'
+import {GlobalStyles} from "./globalStyles"
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <>
+            <GlobalStyles/>
+            <App/>
+        </>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
